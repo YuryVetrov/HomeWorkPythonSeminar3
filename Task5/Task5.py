dbb8fdab171a654ca29440ed4c23efeb555c511b
Vetrov Yury <888vetrov@gmail.com>
2022-07-17 17:36:02 +0300

    Task 5

--- Task5/Task5.py
+++ Task5/Task5.py
@@ -3,3 +3,22 @@
 #Пример:
 # для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+z = int(input('Введите любое число N = '))
+listResult = []
+def fibo(n):
+    if n >= 0:
+       idx = range(n+1)
+       x = [0,1]
+       for k in idx[2:]:
+           x.append(x[k-1] + x[k-2]) 
+       return x[n]
+    else:
+       n =- (n-1)
+       idx = range(n+1)
+       x = [1,0]
+       for k in idx[2:]:
+           x.append(x[k-2] - x[k-1]) 
+       x.reverse()
+    return x[0]
+for i in range(-z, z + 1):
+    print(fibo(i), end=" ")
